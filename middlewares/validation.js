f1afed564f483f3e031c105bc378dd34068d7af2
maxmatyugin <69216530+maxmatyugin@users.noreply.github.com>
2021-05-01 22:31:30 +0300

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -9,7 +9,7 @@ const loginValidation = celebrate({
 
 const registerValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required().unique(),
+    email: Joi.string().email().required(),
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
   }),
